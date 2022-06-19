9d4be3ee18367206fd8b4248fc494b17a9692c83
Collins07 <collins.nyakoe@student.moringaschool.com>
2022-06-19 03:18:42 +0300

    add methods

--- neighbour/models.py
+++ neighbour/models.py
@@ -14,6 +14,10 @@ class NeighbourHood(models.Model):
     police_number = models.IntegerField(null=True)
     date = models.DateTimeField(auto_now_add=True, blank=True, null=True)
 
+    class Meta:
+       ordering = ['-date']
+
+
     def __str__(self):
         return f'{self.name} neighbhood'
 
@@ -22,6 +26,10 @@ class NeighbourHood(models.Model):
 
     def delete_neighborhood(self):
         self.delete()
+
+    @classmethod
+    def all_neighborhoods(cls):
+        return cls.objects.all()    
 
     @classmethod
     def search_neighborhood(cls, neighborhood_id):
