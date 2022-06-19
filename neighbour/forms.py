157b5448cf87ba127aad8096d60773aa4bda2d67
Collins07 <collins.nyakoe@student.moringaschool.com>
2022-06-19 04:14:45 +0300

    user profile form

--- neighbour/forms.py
+++ neighbour/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.db import models
-from .models import Business,Post
+from .models import Business,Post,UserProfile
 from django.contrib.auth.models import User
 
 class BusinessForm(forms.ModelForm):
@@ -11,4 +11,17 @@ class BusinessForm(forms.ModelForm):
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['title', 'post']         
+        fields = ['title', 'post']        
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ['bio', 'profile_picture']
+
+
+class UserForm(forms.ModelForm):
+    email = forms.EmailField(max_length=300, help_text='Required. Inform a valid email address.')
+
+    class Meta:
+        model = User
+        fields = ('username', 'email')          
